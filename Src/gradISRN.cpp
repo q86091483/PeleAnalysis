ac348314fa7431c7688e49341bf3153a24eda045
Zisen Li <zisen347@nia-login01.scinet.local>
2023-08-23 04:51:03 -0400

    Before testing gradient calculatioon.

--- Src/gradISRN.cpp
+++ Src/gradISRN.cpp
@@ -85,47 +85,78 @@ int main (int argc, char* argv[])
   {
     amrex::Print() << plotVarNames[i] << std::endl;
   }
-  int idYin = -1;
-  int idTin = -1;
-  int idRin = -1;
+
   Vector<std::string> spec_names;
   pele::physics::eos::speciesNames<pele::physics::PhysicsType::eos_type>(spec_names);
+
+  int idYin = -1; 
+  int idTin = -1;
+  int idRin = -1;
+  int idZin = -1;
+  int idUin = -1;
+  int idVin = -1;
+  int idWin = -1;
+  const int nCompIn  = NUM_SPECIES + 6;
+
   const std::string spName= "Y(" + spec_names[0] + ")";
   const std::string TName = "temp";
   const std::string RName = "density";
+  const std::string ZName = "mixture_fraction";
+  const std::string UName = "x_velocity";
+  const std::string VName = "y_velocity";
+  const std::string WName = "z_velocity";
   for (int i=0; i<plotVarNames.size(); ++i)
   {
     if (plotVarNames[i] == spName) idYin = i;
     if (plotVarNames[i] == TName)  idTin = i;
     if (plotVarNames[i] == RName)  idRin = i;
+    if (plotVarNames[i] == ZName)  idZin = i;
+    if (plotVarNames[i] == UName)  idUin = i;
+    if (plotVarNames[i] == VName)  idVin = i;
+    if (plotVarNames[i] == WName)  idWin = i;
   }
 
-  const int nCompIn  = NUM_SPECIES + 2;
   const int idDout   = 0;
-  const int idMuout  = idDout + NUM_SPECIES;
-  const int idXiout  = idMuout + 1;
-  const int idLamout = idXiout + 1;
-  const int nCompOut = idLamout + 1;
+  const int idUYout  = idDout   + NUM_SPECIES;
+  const int idMuout  = idUYout  + NUM_SPECIES;
+  const int idXiout  = idMuout  + 1;
+  const int idLamout = idXiout  + 1;
+  const int idZout   = idLamout + 1;
+  const int nCompOut = idZout   + 1;
 
   Vector<std::string> outNames(nCompOut);
   Vector<std::string> inNames(nCompIn);
   Vector<int> destFillComps(nCompIn);
   const int idYlocal = 0; // Xs start here
-  const int idTlocal = NUM_SPECIES;   // T starts here
-  const int idRlocal = NUM_SPECIES+1; // R starts here
+  const int idTlocal = NUM_SPECIES;   // T tempereature starts here
+  const int idRlocal = NUM_SPECIES+1; // R density starts here
+  const int idZlocal = NUM_SPECIES+2; // Z mixture fraction starts here
+  const int idUlocal = NUM_SPECIES+3; // Z mixture fraction starts here
+  const int idVlocal = NUM_SPECIES+4; // Z mixture fraction starts here
+  const int idWlocal = NUM_SPECIES+5; // Z mixture fraction starts here
   for (int i=0; i<NUM_SPECIES; ++i)
   {
     destFillComps[i] = idYlocal + i;
-    inNames[i] =  "Y(" + spec_names[i] + ")";
-    outNames[i] = "rhoD(" + spec_names[i] + ")";
+    inNames[i]          =  "Y(" + spec_names[i] + ")";
+    outNames[i+idDout]  = "rhoD(" + spec_names[i] + ")";
+    outNames[i+idUYout] = "uY(" + spec_names[i] + ")";
   }
   destFillComps[idTlocal] = idTlocal;
   destFillComps[idRlocal] = idRlocal;
+  destFillComps[idZlocal] = idZlocal;
+  destFillComps[idUlocal] = idUlocal;
+  destFillComps[idVlocal] = idVlocal;
+  destFillComps[idWlocal] = idWlocal;
   inNames[idTlocal] = TName;
   inNames[idRlocal] = RName;
-  outNames[NUM_SPECIES  ] = "mu";
-  outNames[NUM_SPECIES+1] = "xi";
-  outNames[NUM_SPECIES+2] = "lambda";
+  inNames[idZlocal] = ZName;
+  inNames[idUlocal] = UName;
+  inNames[idVlocal] = VName;
+  inNames[idWlocal] = WName;
+  outNames[idMuout]   = "mu";
+  outNames[idXiout]   = "xi";
+  outNames[idLamout]  = "lambda";
+  outNames[idZout]    = "mixture_fraction";
 
   Vector<MultiFab*> outdata(Nlev);
   Vector<Geometry> geoms(Nlev);
@@ -140,21 +171,40 @@ int main (int argc, char* argv[])
                               real_box,
                               amrData.CoordSys(),
                               is_periodic);
-  const int nGrow = 0;
+  const int nGrow = 1;
 
   for (int lev=0; lev<Nlev; ++lev)
   {
     const BoxArray ba = amrData.boxArray(lev);
     const DistributionMapping dm(ba);
     outdata[lev] = new MultiFab(ba,dm,nCompOut,nGrow);
+    const Vector<Real>& delta = amrData.DxLevel()[lev];
     if ( lev > 0 ) {
        geoms[lev] = amrex::refine(geoms[lev - 1], 2);
     }
-    MultiFab indata(ba,dm,nCompIn,nGrow);
 
-    Print() << "Reading data for level " << lev << std::endl;
+    MultiFab indata(ba, dm, nCompIn, nGrow);
+    MultiFab chi(ba, dm, NUM_SPECIES, nGrow);
+    MultiFab grads(ba, dm, BL_SPACEDIM+1, nGrow);
+
+    Print() << "Reading data (FillVar) for level " << lev << std::endl;
     amrData.FillVar(indata,lev,inNames,destFillComps);
     Print() << "Data has been read for level " << lev << std::endl;
+
+    // Fix up grow cells.  Use extrap for guess
+    const Box& dbox = amrData.ProbDomain()[lev];
+    for (amrex::MFIter mfi(indata); mfi.isValid(); ++mfi)
+    {
+      FArrayBox& fab = indata[mfi];
+      const Box& box = mfi.validbox();
+      pushvtog(BL_TO_FORTRAN_BOX(box),
+               BL_TO_FORTRAN_BOX(dbox),
+               BL_TO_FORTRAN_ANYD(fab),
+               &nCompIn);
+    }
+
+    // Fix up fine-fine and periodic
+    indata.FillBoundary(geoms[lev].periodicity());
 
     // Get the transport data pointer
     auto const* ltransparm = trans_parms.device_trans_parm();
@@ -168,12 +218,19 @@ int main (int argc, char* argv[])
       const Box& bx = mfi.tilebox();
       Array4<Real> const& Y_a = indata.array(mfi,idYlocal);
       Array4<Real> const& T_a = indata.array(mfi,idTlocal);
+      Array4<Real> const& Z_a = indata.array(mfi,idZlocal);
+      Array4<Real> const& U_a = indata.array(mfi,idUlocal);
+      Array4<Real> const& V_a = indata.array(mfi,idVlocal);
+      Array4<Real> const& W_a = indata.array(mfi,idWlocal);
       Array4<Real> const& rho_a = indata.array(mfi,idRlocal);
+
       Array4<Real> const& D_a = outdata[lev]->array(mfi,idDout);
+      Array4<Real> const& UY_a = outdata[lev]->array(mfi,idUYout);
       Array4<Real> const& mu_a = outdata[lev]->array(mfi,idMuout);
       Array4<Real> const& xi_a = outdata[lev]->array(mfi,idXiout);
       Array4<Real> const& lam_a = outdata[lev]->array(mfi,idLamout);
-      Array4<Real> const& chi_a = outdata[lev]->array(mfi,idLamout+1);
+      Array4<Real> const& Zout_a = outdata[lev]->array(mfi,idZout);
+      Array4<Real> const& chi_a = chi.array(mfi); // Zisen - not sure what chi fields means, so only use a temporaray variable to store it
 
       amrex::launch(bx, [=] AMREX_GPU_DEVICE(amrex::Box const& tbx) {
         auto trans = pele::physics::PhysicsType::transport();
@@ -189,15 +246,27 @@ int main (int argc, char* argv[])
           }
           Real Tlocal = T_a(i,j,k);
           CKCPBS(T_a(i,j,k), Yloc, Cpmix);
-          amrex::Print(ioproc) << D_a(i,j,k,5) << std::endl;
+
+          // convection - <UY|Z>
+          for (int n=0; n<NUM_SPECIES; ++n) {
+              UY_a(i,j,k,n) = U_a(i,j,k) * Y_a(i,j,k,n);
+              UY_a(i,j,k,n) = UY_a(i,j,k,n) + V_a(i,j,k) * Y_a(i,j,k,n);
+              UY_a(i,j,k,n) = UY_a(i,j,k,n) + W_a(i,j,k) * Y_a(i,j,k,n);
+          }
+
+          Zout_a(i,j,k) = Z_a(i,j,k); // Copy mixture fraction.
+          amrex::Print(ioproc) << Z_a(i,j,k) << std::endl;
       });
+
+
 
 
     } // mfi
     Print() << "Derived finished for level " << lev << std::endl;
   } // lev
 
-  std::string outfilename(basename(infile) + "_ISRNderived"); 
+  std::string outDir = "cond_ISRN"; pp.query("outputDir", outDir);
+  std::string outfilename(outDir + "/" + basename(infile) + "_derived"); 
   Print() << "Writing ISRN derived data to " << outfilename << std::endl;
   bool verb = false;
   WritePlotFile(outdata,amrData,outfilename,verb,outNames);
