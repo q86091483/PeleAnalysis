39604e374960512c126faa0fde8e0ff9ad2cbd36
Zisen Li <zisen347@narval3.narval.calcul.quebec>
2024-01-13 18:20:48 -0500

    Plot Pele slices.

--- RJICF/Plot_slices_Ignite.py
+++ RJICF/Plot_slices_Ignite.py
@@ -14,13 +14,13 @@ matplotlib.rcParams['mathtext.fontset'] = 'stix'
 matplotlib.rcParams['font.family'] = 'STIXGeneral'
 
 # Load data
-fn = "/home/zisen347/scratch/scoping_runs/exascale/MicroMix4/plt_04700"
+fn = "/home/zisen347/scratch/scoping_runs/exascale/Ignite/plt_00001"
 ds = yt.load(fn)
 dd = ds.all_data()
 
-xmin = -1.2E-3; xmax = 11.2E-3
-ymin = -2.8E-3; ymax = 2.8E-3
-zmin = -0.0E-3; zmax = 4.0E-3
+xmin = -9.6E-4; xmax = 9.6E-4;
+ymin = -9.6E-4; ymax = 9.6E-4;
+zmin = -4.0E-4; zmax = +4.0E-4;
 Lx = xmax - xmin 
 Ly = ymax - ymin 
 Lz = zmax - zmin
@@ -31,11 +31,12 @@ res_y=res_x * (Ly / Lx)
 res_xz = [res_z, res_x]  # create an image with 1000x1000 pixels
 res_xy = [res_y, res_x]  # create an image with 1000x1000 pixels
 
-loc = 1.4E-3
+loc = 0.0
 fn = "temp"; dir = "y"; vmin=400; vmax=2400
 slc = ds.slice(dir, coord=loc, center=(xmin+0.5*Lx, loc, zmin+0.5*Lz), )
 frb = slc.to_frb(width=((Lz, "cm"),(Lx, "cm")), resolution=res_xz)
 arr_temp = np.array(frb[fn])
+print(np.mean(arr_temp))
 fig, ax = plt.subplots()
 im = ax.imshow(arr_temp.transpose(), 
               origin="lower", cmap="viridis", extent=[xmin, xmax, zmin, zmax],
@@ -45,7 +46,7 @@ cax = divider.append_axes('right', size='5%', pad=0.05)
 fig.colorbar(im, cax=cax, orientation='vertical')
 plt.savefig(fn+"_"+dir+".png", dpi=300, bbox_inches="tight")
 
-fn = "Y(H2)"; dir = "y"; vmin = 0.0; vmax = 1.0
+fn = "temp"; dir = "y"; vmin = 0.0; vmax = 2400
 slc = ds.slice(dir, coord=loc, center=(xmin+0.5*Lx, loc, zmin+0.5*Lz), )
 frb = slc.to_frb(width=((Lz, "cm"),(Lx, "cm")), resolution=res_xz)
 arr_temp = np.array(frb[fn])
@@ -56,7 +57,7 @@ cax = divider.append_axes('right', size='5%', pad=0.05)
 fig.colorbar(im, cax=cax, orientation='vertical')
 plt.savefig(fn+"_"+dir+".png", dpi=300, bbox_inches="tight")
 
-fn = "mixture_fraction"; dir = "y"; vmin = 0.0; vmax = 1.0
+fn = "x_velocity"; dir = "y"; vmin = -50; vmax = 50
 slc = ds.slice(dir, coord=loc, center=(xmin+0.5*Lx, loc, zmin+0.5*Lz), )
 frb = slc.to_frb(width=((Lz, "cm"),(Lx, "cm")), resolution=res_xz)
 arr_temp = np.array(frb[fn])
@@ -67,32 +68,8 @@ cax = divider.append_axes('right', size='5%', pad=0.05)
 fig.colorbar(im, cax=cax, orientation='vertical')
 plt.savefig(fn+"_"+dir+".png", dpi=300, bbox_inches="tight")
 
-fn = "HeatRelease"; dir = "y"; vmin = 1E5; vmax = 1E12
-slc = ds.slice(dir, coord=loc, center=(xmin+0.5*Lx, loc, zmin+0.5*Lz), )
-frb = slc.to_frb(width=((Lz, "cm"),(Lx, "cm")), resolution=res_xz)
-arr_temp = np.array(frb[fn])
-fig, ax = plt.subplots()
-phi = np.log10(np.abs(arr_temp.transpose()))
-phi = arr_temp.transpose()
-im = ax.imshow(phi, vmin=vmin, vmax=vmax, origin="lower", cmap="hot", extent=[xmin, xmax, zmin, zmax])
-divider = make_axes_locatable(ax)
-cax = divider.append_axes('right', size='5%', pad=0.05)
-fig.colorbar(im, cax=cax, orientation='vertical')
-plt.savefig(fn+"_"+dir+".png", dpi=300, bbox_inches="tight")
 
-
-fn = "x_velocity"; dir = "y"; vmin = -100; vmax = 100
-slc = ds.slice(dir, coord=loc, center=(xmin+0.5*Lx, loc, zmin+0.5*Lz), )
-frb = slc.to_frb(width=((Lz, "cm"),(Lx, "cm")), resolution=res_xz)
-arr_temp = np.array(frb[fn])
-fig, ax = plt.subplots()
-im = ax.imshow(arr_temp.transpose(), vmin=vmin, vmax=vmax, origin="lower", cmap="viridis", extent=[xmin, xmax, zmin, zmax])
-divider = make_axes_locatable(ax)
-cax = divider.append_axes('right', size='5%', pad=0.05)
-fig.colorbar(im, cax=cax, orientation='vertical')
-plt.savefig(fn+"_"+dir+".png", dpi=300, bbox_inches="tight")
-
-fn = "Y(H2)"; dir = "x"; loc = 1.0E-3
+fn = "Y(H2)"; dir = "x"; loc = 8.0E-4
 slc = ds.slice(dir, coord=loc, center=(loc, ymin+0.5*Ly, zmin+0.5*Lz), )
 frb = slc.to_frb(width=((Ly, "cm"),(Lz, "cm")), resolution=res_xy)
 arr_temp = np.array(frb[fn])
@@ -103,21 +80,15 @@ cax = divider.append_axes('right', size='5%', pad=0.05)
 fig.colorbar(im, cax=cax, orientation='vertical')
 plt.savefig(fn+"_"+dir+".png", dpi=300, bbox_inches="tight")
 
-fn = "temp"; dir = "z"; loc = 0.001
+fn = "temp"; dir = "z"; loc = 0.0000
 slc = ds.slice(dir, coord=loc, center=(xmin+0.5*Lx, ymin+0.5*Ly, loc), )
 frb = slc.to_frb(width=((Lx, "cm"),(Ly, "cm")), resolution=res_xy)
 arr_temp = np.array(frb[fn])
 fig, ax = plt.subplots()
-im = ax.imshow(arr_temp.transpose(), origin="lower", cmap="hot", extent=[ymin, ymax, xmin, xmax])
+im = ax.imshow(arr_temp.transpose(), origin="lower", cmap="viridis", extent=[ymin, ymax, xmin, xmax])
 divider = make_axes_locatable(ax)
 cax = divider.append_axes('right', size='5%', pad=0.05)
 fig.colorbar(im, cax=cax, orientation='vertical')
 plt.savefig(fn+"_"+dir+".png", dpi=300, bbox_inches="tight")
-
-#%%
-#ds = yt.load("/home/zisen347/scratch/scoping_runs/exascale/MicroMix4/chk_04360")
-#slc = yt.SlicePlot(ds, "y", ("gas", "Y(H2)"), width=((Lx, "cm"),(Ly, "cm")), center="m")
-#slc.annotate_cell_edges()
-#slc.save()
 
 #%%
