a36d344d42f70d31ce9a3cfbbaca4d088008477b
Zisen Li <zisen347@nia-login07.scinet.local>
2024-04-13 22:51:42 -0400

    Add PrintData_slice2D.py

---
+++ Py-pelelmex/PrintData_slice2D.py
@@ -0,0 +1,18 @@
+#%%
+import sys
+path_PeleAnalysis = os.path.abspath("..")
+sys.path.append(path_PeleAnalysis)
+from amr_kitchen.mandoline import Mandoline
+from amr_kitchen import HeaderData
+import glob
+
+# Input
+# Where plt files are stored
+plt_folder = "/scratch/b/bsavard/zisen347/scopingRuns/MicroMix"
+# Case name
+case_name = "Micromix"
+# 
+
+
+
+#%%
